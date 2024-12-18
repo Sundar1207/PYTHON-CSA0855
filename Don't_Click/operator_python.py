7f33a5cbb5a11dce9f402d1fe7b81a43c972610a
S.Sundaramoorthy <157666440+Sundar1207@users.noreply.github.com>
2024-12-18 09:07:46 +0530

    Create operator_python.py

---
+++ Don't_Click/operator_python.py
@@ -0,0 +1,26 @@
+a=8
+b=12
+c=a+b   #->add between two number
+print(c)
+c=a-b  #-> subtraction between two number
+print(c)
+c=a*b    #-> multiplication between two number
+print(c)
+c=a/b    #-> divition between two number
+print(c)
+c=a**b    #-> a power b ,using two (**) 
+print(c)
+c=a//b    #->  a div b give  the real nuymber , using two(//) 5//2 => 2 it value 
+print(c)
+c=a%b    #->  a Modulus b 
+print(c)
+
+
+print(a<<1) # -> 1000  one digit 0 add in right site  in binary method  a=16
+print(a>>1) # -> 1000  one digit 0 add in left site  in binary method  a=4
+
+print(a==b)   #-> a equal to b it is equal print true not equle false
+print(a!=b)   #-> a not equal to b it is not equal print true  but equal print false 
+
+print(x:=3) # -> initialization body of print 
+
