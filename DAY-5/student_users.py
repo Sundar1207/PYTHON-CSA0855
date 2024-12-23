040658d2dbd244dbf6d50ccab0bc1b0c7b767bc0
S.Sundaramoorthy <157666440+Sundar1207@users.noreply.github.com>
2024-12-23 14:16:45 +0530

    Delete DAY-5 directory

--- DAY-5/student_users.py
+++
@@ -1,5 +0,0 @@
-total_users = int(input("Total Users: "))
-staff_users = int(input("Staff Users: "))
-non_teaching_staff = staff_users // 3
-student_users = total_users - staff_users - non_teaching_staff
-print("Student Users:", student_users)
