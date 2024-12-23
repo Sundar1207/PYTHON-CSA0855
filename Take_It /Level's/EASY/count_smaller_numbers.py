3d8dd785aa5fc2fe858d93b3c1c4aaf233cccf11
S.Sundaramoorthy <157666440+Sundar1207@users.noreply.github.com>
2024-12-23 14:27:44 +0530

    Delete Take_It /Level's/EASY directory

--- Take_It /Level's/EASY/count_smaller_numbers.py
+++
@@ -1,7 +0,0 @@
-nums = list(map(int, input("Enter numbers separated by spaces: ").split()))
-result = []
-
-for i in nums:
-    result.append(sum(1 for j in nums if j < i))
-
-print("Output:", result)
