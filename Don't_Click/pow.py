307f652feaa200483e6bf34b141563b4d2111520
S.Sundaramoorthy <157666440+Sundar1207@users.noreply.github.com>
2024-12-23 14:42:21 +0530

    Delete Don't_Click directory

--- Don't_Click/pow.py
+++
@@ -1,4 +0,0 @@
-import sundar
-
-print(sundar.power(2))
-print(sundar.palindrome(121))
--- Don't_Click/sundar.py
+++
@@ -1,8 +0,0 @@
-def power(n):
-    return n**2
-def palindrome(n):
-    x=str(n)
-    if(x==x[::-1]):
-        return True
-    else:
-        return False
